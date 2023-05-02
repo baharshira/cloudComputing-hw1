4b5975d70b672e0b882348c65097fb0384f29b21
Shira Bahar <shira.bahar@post.runi.ac.il>
2023-05-02 10:37:10 +0300

    Code without DB

--- parkingLotApp.py
+++ parkingLotApp.py
@@ -1,100 +1,76 @@
-import boto3
-import json
-import datetime
+from datetime import datetime, timedelta
+from flask import Flask, request
 
-# Initialize AWS services
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('parking')
+import app as app
 
 
-def lambda_handler(event, context):
-    if event['httpMethod'] == 'POST' and event['resource'] == '/entry':
-        # Process entry request
-        query_params = event['queryStringParameters']
-        license_plate = query_params['plate']
-        parking_lot_id = query_params['parkingLot']
-        entry_time = datetime.datetime.now().isoformat()
+class Ticket:
+    app = Flask(__name__)
 
-        # Insert entry data into DynamoDB
-        item = {
-            'license_plate': license_plate,
-            'entry_time': entry_time,
-            'parking_lot_id': parking_lot_id
-        }
-        table.put_item(Item=item)
+    def __init__(self, plate, parkingLot, ticketId, entryTime):
+        self.plate = plate
+        self.parkingLot = parkingLot
+        self.ticketId = ticketId
+        self.entryTime = entryTime
 
-        # Return ticket ID
-        ticket_id = str(item['entry_time']) + '-' + license_plate
-        response = {
-            'statusCode': 200,
-            'body': json.dumps({'ticketId': ticket_id})
-        }
-        return response
+    # initialize a global list to store created tickets
+    tickets = []
+    # initialize a global variable to keep track of the last assigned ticket ID
+    last_ticket_id = 0
 
-    elif event['httpMethod'] == 'POST' and event['resource'] == '/exit':
-        # Process exit request
-        query_params = event['queryStringParameters']
-        ticket_id = query_params['ticketId']
-        exit_time = datetime.datetime.now().isoformat()
+    # example Flask route for handling POST requests to /entry
+    @app.route('/entry', methods=['POST'])
+    def create_ticket(self):
+        global last_ticket_id
 
-        # Retrieve entry data from DynamoDB
-        response = table.get_item(Key={'ticket_id': ticket_id})
-        entry_data = response.get('Item', None)
+        # extract the plate and parkingLot values from the request parameters
+        plate = request.args.get('plate')
+        parking_lot = request.args.get('parkingLot')
 
-        if entry_data:
-            license_plate = entry_data['license_plate']
-            entry_time = entry_data['entry_time']
-            parking_lot_id = entry_data['parking_lot_id']
-            entry_time = datetime.datetime.fromisoformat(entry_time)
-            exit_time = datetime.datetime.fromisoformat(exit_time)
+        # save the current time as the entry time for the ticket
+        entry_time = datetime.now().isoformat()
 
-            # Calculate parking charge
-            charge = calculate_charge(entry_time, exit_time)
+        # generate a unique ticket ID based on the last assigned ticket ID
+        ticket_id = last_ticket_id + 1
+        last_ticket_id = ticket_id
 
-            # Update exit time and charge in DynamoDB
-            table.update_item(
-                Key={'ticket_id': ticket_id},
-                UpdateExpression='SET exit_time = :exit_time, charge = :charge',
-                ExpressionAttributeValues={
-                    ':exit_time': exit_time.isoformat(),
-                    ':charge': charge
-                }
-            )
+        # create a new ticket object with the provided data and entry time
+        ticket = Ticket(plate, parking_lot, ticket_id, entry_time)
 
-            # Return parking details
-            response = {
-                'statusCode': 200,
-                'body': json.dumps({
-                    'licensePlate': license_plate,
-                    'totalParkedTime': str(exit_time - entry_time),
-                    'parkingLotId': parking_lot_id,
+        # save the ticket to the global list
+        Ticket.tickets.append(ticket)
+
+        # return a success response
+        return {'status': 'success', 'ticket': ticket.__dict__}
+
+    @app.route('/exit', methods=['POST'])
+    def exit_parking(self):
+        # extract the ticket ID from the request parameters
+        ticket_id = int(request.args.get('ticketId'))
+
+        # find the corresponding ticket object in the list of tickets
+        for ticket in Ticket.tickets:
+            if ticket.ticketId == ticket_id:
+                # calculate the parking time based on the difference between the entry time and the current time
+                entry_time = datetime.fromisoformat(ticket.entryTime)
+                exit_time = datetime.now()
+                parking_time = exit_time - entry_time
+                parking_hours = parking_time.total_seconds() / 3600
+
+                # calculate the charge based on the parking time
+                charge = round(parking_hours * 10, 2)
+
+                # remove the ticket from the list of tickets
+                Ticket.tickets.remove(ticket)
+
+                # return the license plate, parking time, parking lot ID, and charge as a response
+                return {
+                    'status': 'success',
+                    'plate': ticket.plate,
+                    'parkingTime': str(parking_time),
+                    'parkingLot': ticket.parkingLot,
                     'charge': charge
-                })
-            }
-            return response
-        else:
-            # Ticket ID not found in DynamoDB
-            response = {
-                'statusCode': 404,
-                'body': json.dumps({'error': 'Ticket ID not found'})
-            }
-            return response
+                }
 
-    else:
-        # Invalid resource or HTTP method
-        response = {
-            'statusCode': 404,
-            'body': json.dumps({'error': 'Invalid resource or HTTP method'})
-        }
-        return response
-
-
-def calculate_charge(entry_time, exit_time):
-    """
-    Calculate the parking charge based on the entry and exit time.
-    Charge is $10 per hour, rounded up to the nearest 15 minutes.
-    """
-    time_difference = exit_time - entry_time
-    hours = time_difference.total_seconds() / 3600
-    charge = round((hours * 10), 2)
-    return charge
+        # if the ticket ID was not found, return an error response
+        return {'status': 'error', 'message': 'Ticket ID not found'}
