035ea3b41754298386341a9b0b47d38552f71fec
Shira Bahar <shira.bahar@post.runi.ac.il>
2023-05-03 11:57:02 +0300

    Working app

--- parkingLotApp.py
+++ parkingLotApp.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from flask import Flask, request
-import app as app
 
 app = Flask(__name__)
 
@@ -11,64 +10,64 @@ class Ticket:
         self.ticketId = ticketId
         self.entryTime = entryTime
 
-    # initialize a global list to store created tickets
-    tickets = []
-    # initialize a global variable to keep track of the last assigned ticket ID
-    last_ticket_id = 0
+# initialize a global list to store created tickets
+tickets = []
+# initialize a global variable to keep track of the last assigned ticket ID
+last_ticket_id = 0
 
-    # example Flask route for handling POST requests to /entry
-    @app.route('/entry', methods=['POST'])
-    def create_ticket(self):
-        global last_ticket_id
+# example Flask route for handling POST requests to /entry
+@app.route('/entry', methods=['POST'])
+def create_ticket():
+    global last_ticket_id
 
-        # extract the plate and parkingLot values from the request parameters
-        plate = request.args.get('plate')
-        parking_lot = request.args.get('parkingLot')
+    # extract the plate and parkingLot values from the request parameters
+    plate = request.args.get('plate')
+    parking_lot = request.args.get('parkingLot')
 
-        # save the current time as the entry time for the ticket
-        entry_time = datetime.now().isoformat()
+    # save the current time as the entry time for the ticket
+    entry_time = datetime.now().isoformat()
 
-        # generate a unique ticket ID based on the last assigned ticket ID
-        ticket_id = last_ticket_id + 1
-        last_ticket_id = ticket_id
+    # generate a unique ticket ID based on the last assigned ticket ID
+    ticket_id = last_ticket_id + 1
+    last_ticket_id = ticket_id
 
-        # create a new ticket object with the provided data and entry time
-        ticket = Ticket(plate, parking_lot, ticket_id, entry_time)
+    # create a new ticket object with the provided data and entry time
+    ticket = Ticket(plate, parking_lot, ticket_id, entry_time)
 
-        # save the ticket to the global list
-        Ticket.tickets.append(ticket)
+    # save the ticket to the global list
+    tickets.append(ticket)
 
-        # return a success response
-        return {'status': 'success', 'ticket': ticket.__dict__}
+    # return a success response
+    return {'status': 'success', 'ticket': ticket.ticketId, 'plate':ticket.plate}
 
-    @app.route('/exit', methods=['POST'])
-    def exit_parking(self):
-        # extract the ticket ID from the request parameters
-        ticket_id = int(request.args.get('ticketId'))
+@app.route('/exit', methods=['POST'])
+def exit_parking():
+    # extract the ticket ID from the request parameters
+    ticket_id = int(request.args.get('ticketId'))
 
-        # find the corresponding ticket object in the list of tickets
-        for ticket in Ticket.tickets:
-            if ticket.ticketId == ticket_id:
-                # calculate the parking time based on the difference between the entry time and the current time
-                entry_time = datetime.fromisoformat(ticket.entryTime)
-                exit_time = datetime.now()
-                parking_time = exit_time - entry_time
-                parking_hours = parking_time.total_seconds() / 3600
+    # find the corresponding ticket object in the list of tickets
+    for ticket in tickets:
+        if ticket.ticketId == ticket_id:
+            # calculate the parking time based on the difference between the entry time and the current time
+            entry_time = datetime.fromisoformat(ticket.entryTime)
+            exit_time = datetime.now()
+            parking_time = exit_time - entry_time
+            parking_hours = parking_time.total_seconds() / 3600
 
-                # calculate the charge based on the parking time
-                charge = round(parking_hours * 10, 2)
+            # calculate the charge based on the parking time
+            charge = round(parking_hours * 10, 2)
 
-                # remove the ticket from the list of tickets
-                Ticket.tickets.remove(ticket)
+            # remove the ticket from the list of tickets
+            tickets.remove(ticket)
 
-                # return the license plate, parking time, parking lot ID, and charge as a response
-                return {
-                    'status': 'success',
-                    'plate': ticket.plate,
-                    'parkingTime': str(parking_time),
-                    'parkingLot': ticket.parkingLot,
-                    'charge': charge
-                }
+            # return the license plate, parking time, parking lot ID, and charge as a response
+            return {
+                'status': 'success',
+                'plate': ticket.plate,
+                'parkingTime': str(parking_time),
+                'parkingLot': ticket.parkingLot,
+                'charge': charge
+            }
 
-        # if the ticket ID was not found, return an error response
-        return {'status': 'error', 'message': 'Ticket ID not found'}
+    # if the ticket ID was not found, return an error response
+    return {'status': 'error', 'message': 'Ticket ID not found'}
